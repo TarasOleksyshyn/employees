f2dffc80e5c395d5316794d1dd7aff5307ba65c7
Taras Oleksyshyn <x3mal18@gmail.com>
2022-07-31 17:37:30 +0300

    fixed the construction of the list from the 'data'

--- src/components/employees-list/eployees-list.js
+++ src/components/employees-list/eployees-list.js
@@ -2,13 +2,17 @@ import EmployeesListItem from '../employees-list-item/employees-list-item';
 
 import './eployees-list.css';
 
-function EployeesList() {
+function EployeesList({ data }) {
+
+  const elements = data.map(item => {
+    return (
+      <EmployeesListItem {...item} />
+    );
+  });
+
   return (
     <ul className="app-list list-group">
-      <EmployeesListItem name="Ken Dust" salary="1000$" />
-      <EmployeesListItem name="Mary Jhones" salary="1200$" />
-      <EmployeesListItem name="John Smith" salary="2000$" />
-      <EmployeesListItem name="Andrew Bigins" salary="800$" />
+      {elements}
     </ul>
   );
 }
