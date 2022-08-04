33e9885ece4a0a7bc6d736af3860fa21901a49b2
Taras Oleksyshyn <x3mal18@gmail.com>
2022-08-04 15:33:08 +0300

    onDelete was added for for component

--- src/components/employees-list-item/employees-list-item.js
+++ src/components/employees-list-item/employees-list-item.js
@@ -3,57 +3,60 @@ import { Component } from 'react';
 import './employees-list-item.css';
 
 class EmployeesListItem extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      increase: false,
-      rise: false,
-    }
-  }
-  onChangeIncrease = () => {
-    this.setState(({ increase }) => ({
-      increase: !increase
-    }))
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			increase: false,
+			rise: false,
+		}
+	}
+	onChangeIncrease = () => {
+		this.setState(({ increase }) => ({
+			increase: !increase
+		}))
+	}
 
-  onRise = () => {
-    this.setState(({ rise }) => ({
-      rise: !rise
-    }))
-  }
+	onRise = () => {
+		this.setState(({ rise }) => ({
+			rise: !rise
+		}))
+	}
 
-  render() {
-    const { name, salary } = this.props;
-    const { increase, rise } = this.state;
-    let classNames = "list-group-item d-flex justify-content-between";
-    if (increase) {
-      classNames += " increase";
-    }
-    if (rise) {
-      classNames += " like";
-    }
+	render() {
+		const { name, salary, onDelete } = this.props;
+		const { increase, rise } = this.state;
+		let classNames = "list-group-item d-flex justify-content-between";
+		if (increase) {
+			classNames += " increase";
+		}
+		if (rise) {
+			classNames += " like";
+		}
 
-    return (
-      <li className={classNames}>
-        <span className="list-group-item-label" onClick={this.onRise}>{name}</span>
-        <input type="text" className="list-group-item-input" defaultValue={salary + ' $'} />
-        <div className='d-flex justify-content-center align-items-center'>
-          <button type="button"
-            className="btn-cookie btn-sm "
-            onClick={this.onChangeIncrease}
-          >
-            <i className="fas fa-cookie"></i>
-          </button>
+		return (
+			<li className={classNames}>
+				<span className="list-group-item-label" onClick={this.onRise}>{name}</span>
+				<input type="text" className="list-group-item-input" defaultValue={salary + ' $'} />
+				<div className='d-flex justify-content-center align-items-center'>
+					<button type="button"
+						className="btn-cookie btn-sm "
+						onClick={this.onChangeIncrease}
+					>
+						<i className="fas fa-cookie"></i>
+					</button>
 
-          <button type="button"
-            className="btn-trash btn-sm ">
-            <i className="fas fa-trash"></i>
-          </button>
-          <i className="fas fa-star"></i>
-        </div>
-      </li>
-    );
-  }
+					<button
+						type="button"
+						className="btn-trash btn-sm "
+						onClick={onDelete}
+					>
+						<i className="fas fa-trash"></i>
+					</button>
+					<i className="fas fa-star"></i>
+				</div>
+			</li>
+		);
+	}
 }
 
 export default EmployeesListItem;
--- src/components/employees-list/eployees-list.js
+++ src/components/employees-list/eployees-list.js
@@ -2,21 +2,25 @@ import EmployeesListItem from '../employees-list-item/employees-list-item';
 
 import './eployees-list.css';
 
-function EployeesList({ data }) {
+function EployeesList({ data, onDelete }) {
 
-  const elements = data.map(item => {
-    const { id, ...itemProps } = item;
+	const elements = data.map(item => {
+		const { id, ...itemProps } = item;
 
-    return (
-      <EmployeesListItem key={id} {...itemProps} />
-    );
-  });
+		return (
+			<EmployeesListItem
+				key={id}
+				{...itemProps}
+				onDelete={() => onDelete(id)}
+			/>
+		);
+	});
 
-  return (
-    <ul className="app-list list-group">
-      {elements}
-    </ul>
-  );
+	return (
+		<ul className="app-list list-group">
+			{elements}
+		</ul>
+	);
 }
 
 export default EployeesList;
