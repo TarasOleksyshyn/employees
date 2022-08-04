b6cd03b5c691f22d1f2b82a1c1abc34dc58f1894
Taras Oleksyshyn <x3mal18@gmail.com>
2022-08-04 15:30:33 +0300

    added form submit lisener

--- src/components/employees-add-form/employees-add-form.js
+++ src/components/employees-add-form/employees-add-form.js
@@ -18,6 +18,14 @@ class EmployeesAddForm extends Component {
 		})
 	}
 
+	onSubmit = (e) => {
+		e.preventDefault(this.props.onAdd(this.state.name, this.state.salary));
+		this.setState({
+			name: '',
+			salary: '',
+		})
+	}
+
 	render() {
 		const { name, salary } = this.state;
 
@@ -25,7 +33,9 @@ class EmployeesAddForm extends Component {
 			<div className="app-add-form">
 				<h3>Add a new employee</h3>
 				<form
-					className="add-form d-flex">
+					className="add-form d-flex"
+					onSubmit={this.onSubmit}
+				>
 					<input type="text"
 						className="form-control new-post-label"
 						placeholder="What's the employee's name?"
@@ -41,8 +51,12 @@ class EmployeesAddForm extends Component {
 						onChange={this.onValueChange}
 					/>
 
-					<button type="submit"
-						className="btn btn-outline-light">Add</button>
+					<button
+						type="submit"
+						className="btn btn-outline-light"
+					>
+						Add
+					</button>
 				</form>
 			</div>
 		)
