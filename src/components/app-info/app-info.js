2e76b232ed1bf521621a6ea95b133263d2b58be8
Taras Oleksyshyn <x3mal18@gmail.com>
2022-08-05 18:41:35 +0300

    added props to component

--- src/components/app-info/app-info.js
+++ src/components/app-info/app-info.js
@@ -1,14 +1,14 @@
 
 import './app-info.css';
 
-function AppInfo() {
-  return (
-    <div className="app-info">
-      <h1>Accounting of employees in the company X</h1>
-      <h2>Total number of employees: {0}</h2>
-      <h2>The award will be received:</h2>
-    </div>
-  );
+function AppInfo({ companyName, employeesTotal, employeesIncreased }) {
+    return (
+        <div className="app-info">
+            <h1>Accounting of employees in the company<br /> {companyName}</h1>
+            <h2>Total number of employees: {employeesTotal}</h2>
+            <h2>The award will be received: {employeesIncreased}</h2>
+        </div>
+    );
 }
 
 export default AppInfo;
