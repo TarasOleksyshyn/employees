1f518c59f4ede31c199ed220abd3f91030372662
Taras Oleksyshyn <x3mal18@gmail.com>
2022-08-05 18:45:56 +0300

    remastered SearchPanel from functional to class

--- src/components/search-panel/search-panel.js
+++ src/components/search-panel/search-panel.js
@@ -1,14 +1,23 @@
+import { Component } from 'react';
 
 import './search-panel.css';
 
-function SearchPanel() {
-  return (
-    <input
-      type="text"
-      className="form-control search-input"
-      placeholder="Find an employee"
-    />
-  );
+class SearchPanel extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            term: ''
+        }
+    }
+    render() {
+        return (
+            <input
+                type="text"
+                className="form-control search-input"
+                placeholder="Find an employee"
+            />
+        );
+    }
 }
 
 export default SearchPanel;
