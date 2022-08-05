5e9af2f23a6272850536fba0922fff87a9ce3b0b
Taras Oleksyshyn <x3mal18@gmail.com>
2022-08-05 18:46:17 +0300

    fixed tabs

--- src/components/app-filter/app-filter.js
+++ src/components/app-filter/app-filter.js
@@ -2,28 +2,28 @@
 import './app-filter.css';
 
 function AppFilter() {
-  return (
-    <div className="btn-group">
-      <button
-        className="btn btn-light"
-        type="btn"
-      >
-        All employees
-      </button>
-      <button
-        className="btn btn-outline-light"
-        type="btn"
-      >
-        On promotion
-      </button>
-      <button
-        className="btn btn-outline-light"
-        type="btn"
-      >
-        Salary more 1000$
-      </button>
-    </div>
-  );
+    return (
+        <div className="btn-group">
+            <button
+                className="btn btn-light"
+                type="btn"
+            >
+                All employees
+            </button>
+            <button
+                className="btn btn-outline-light"
+                type="btn"
+            >
+                On promotion
+            </button>
+            <button
+                className="btn btn-outline-light"
+                type="btn"
+            >
+                Salary more 1000$
+            </button>
+        </div>
+    );
 }
 
 export default AppFilter;
