92f85f686e79d5bf0892d0027991174737c70755
Taras Oleksyshyn <x3mal18@gmail.com>
2022-08-06 12:14:05 +0300

    filter component was remastered

--- src/components/app-filter/app-filter.js
+++ src/components/app-filter/app-filter.js
@@ -1,27 +1,29 @@
 
 import './app-filter.css';
 
-function AppFilter() {
+function AppFilter(props) {
+    const buttonsData = [
+        { name: 'all', label: 'All employees' },
+        { name: 'rise', label: 'On promotion' },
+        { name: 'moreThen1000', label: 'Salary more 1000$' },
+    ]
+    const buttons = buttonsData.map(({ name, label }) => {
+        const active = props.filter === name;
+        const clazz = active ? "btn-light" : "btn-outline-light";
+        return (
+            <button
+                className={`btn ${clazz}`}
+                type="btn"
+                key={name}
+                onClick={() => props.onFilterSelect(name)}
+            >
+                {label}
+            </button >
+        )
+    })
     return (
         <div className="btn-group">
-            <button
-                className="btn btn-light"
-                type="btn"
-            >
-                All employees
-            </button>
-            <button
-                className="btn btn-outline-light"
-                type="btn"
-            >
-                On promotion
-            </button>
-            <button
-                className="btn btn-outline-light"
-                type="btn"
-            >
-                Salary more 1000$
-            </button>
+            {buttons}
         </div>
     );
 }
